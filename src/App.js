83e41112294947267b3a1ac3b20ac5f8431c981b
Alexander <87819638+aleksandr24789@users.noreply.github.com>
2024-11-03 11:30:11 +0500

    Add loading pizzas from mock api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizza.json';
 
 function App() {
+  const [pizzaItems, setPizzaItems] = useState([]);
+  const pizzaApi = 'https://67270754302d03037e6f186e.mockapi.io/items';
+
+  const getPizzas = async () => {
+    try {
+      const response = await fetch(pizzaApi);
+
+      if (response.ok) {
+        const pizzaArr = await response.json();
+        setPizzaItems(pizzaArr);
+      }
+    } catch (error) {
+      alert(`Ошибка при загрузке данных: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getPizzas();
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -18,7 +38,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((pizzaItem, index) => {
+              {pizzaItems.map((pizzaItem) => {
                 return <PizzaBlock {...pizzaItem} key={pizzaItem.id} />;
               })}
             </div>
