7239ceca3ea1583d06017646d3f8bf5206626c13
Alexander <87819638+aleksandr24789@users.noreply.github.com>
2024-11-24 17:14:12 +0500

    Update CartItem with changing cart counts

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -6,6 +6,11 @@ const initialState = {
   totalPrice: 0,
 };
 
+const recalculateTotals = (state) => {
+  state.totalCount = state.items.reduce((sum, item) => sum + item.count, 0);
+  state.totalPrice = state.items.reduce((sum, item) => item.price * item.count + sum, 0);
+};
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
@@ -24,12 +29,17 @@ const cartSlice = createSlice({
           count: 1,
         });
       }
+      recalculateTotals(state);
+    },
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
 
-      state.totalCount = state.items.reduce((sum, item) => sum + item.count, 0);
-
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count + sum;
-      }, 0);
+      if (findItem && findItem.count > 1) findItem.count--;
+      recalculateTotals(state);
+    },
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+      recalculateTotals(state);
     },
   },
 });
@@ -40,6 +50,6 @@ export const selectItemById = (id) => (state) => state.cart.items.find((obj) => 
 export const selectTotalCount = (state) => state.cart.totalCount;
 export const selectTotalPrice = (state) => state.cart.totalPrice;
 
-export const { clearItems, addItem } = cartSlice.actions;
+export const { clearItems, addItem, minusItem, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
